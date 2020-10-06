b06176cf42223471ef599c7c67029e8ded647ab9
Skander <skander@bibani.nl>
2020-10-06 15:01:43 +0200

    Add initial state

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -6,11 +6,25 @@ import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initViews()
+    }
+
+    fun initViews() {
+        updateUI()
+    }
+
+    fun updateUI() {
+        // 1. Access view through binding.
+        // 2. Set its text property to the string from res>strings.xml
+        // (R stands for the resource folder)
+        binding.lastThrowValue.text = getString(R.string.last_throw, lastThrow)
     }
 }
